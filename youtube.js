3fad41f414f832d1149bfecc5f6117201e53ef60
liziAmong <20light02@gmail.com>
2025-07-01 00:09:47 +0900

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,5 +1,19 @@
 const videoIds = [
-  "TkzT-8xNaP8"
+  "TkzT-8xNaP8", //너에게 닿기를
+  "G33eHNor1Kc", //자코자코
+  "48tTGn9QNHA", // 바니
+  "Ydvct6uheHI", //소원을말해봐 
+  "uI53kboOem0", //디토 
+  "pSXa7732k4U",//써머타임 
+  "NCO8v89pNDs",//비비디
+  "aMv1jbqdHY4",//연예서쿨레이션
+  "NCWwaBDlMnE",//로리신 
+  "n1ly5P-DTEU",//디스코
+  "NZK5wT7rq7E",//아이아이
+  "wFa87J4vfik",//기상야자
+  "JRttCcjxzQY",//데몬로드
+  "ytHiCavTqKQ",//사인은B
+  "9mNDxlE3lbA",//아이돌
 ];
 
 let currentIndex = 0;
