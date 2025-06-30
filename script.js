bf4cc45b606ddf1488c8ee1e817b27f00e1b28fa
liziAmong <20light02@gmail.com>
2025-07-01 00:15:41 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const images = [];
 const sounds = [];
-const totalItems = 20; // 필요한 이미지+사운드 개수
+const totalItems = 20;
 
 for (let i = 1; i <= totalItems; i++) {
   images.push(`images/image${i}.jpg`);
@@ -30,11 +30,9 @@ function updateDisplay() {
   imageElement.src = images[currentIndex];
 }
 
-// 자동 슬라이드 기능 (5초마다)
 setInterval(() => {
   currentIndex = (currentIndex + 1) % images.length;
   updateDisplay();
 }, 5000);
 
-// 초기 표시
 updateDisplay();
